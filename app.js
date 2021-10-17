b4d60eede450b0842bbb1072d2d62d34b3e97e30
ryang <ryan.r.c.339ac@gmail.com>
2021-10-17 11:48:05 -0300

    now the pokemon's name is beeing saved and showed in the console

--- app.js
+++ app.js
@@ -3,5 +3,24 @@ const fetchPokemon = () =>{
     
     const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`//GETs API's url
 
-}
+    const pokemonPromises = []//A array to storage the promises and show in the screen
+
+    //Each pokemon needs its own request
+    for(let i = 1; i <= 151; i++){
+        pokemonPromises.push(fetch(getPokemonUrl(i))//modern method to do AJAX requests! -> .:. It gets data from the url. So returns a promise
+        .then(response => response.json())//Change the response from HTTP to JSON. But it so returns a promise
+        )//storage the pokemon data in the array
+        Promise.all(pokemonPromises)//after all pokemon promises be done it will
+        .then(pokemons => {
+            const listPokemon = pokemons.reduce((accumulator, pokemon) => {// transforms a array into a string
+                accumulator += `<li>${pokemon.name}</li>`
+                return accumulator
+            }, '')//after do the create a li with pokemon data it will be none
+            console.log(listPokemon)
+        },
+
+        )
+
+    }
+} 
 fetchPokemon()
