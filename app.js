c680622ad978b1edd1fdfe0131db5b2b78f30178
ryang <ryan.r.c.339ac@gmail.com>
2021-10-17 16:29:58 -0300

    commented the code

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ const fetchPokemon = () =>{
         Promise.all(pokemonPromises)//after all pokemon promises be done it will
         .then(pokemons => {
             const listPokemon = pokemons.reduce((accumulator, pokemon) => {// transforms a array into a string
-                const types = pokemon.types.map(typeinfo => typeinfo.type.name)
+                const types = pokemon.types.map(typeinfo => typeinfo.type.name)//gets the type(s) of the pokemon, made it like this to save code
+                //this accumulator will get all the pokemon data and interpolate it in HTML
                 accumulator += `
                     <li class="card ${types[0]}">
                         <img class="card-image" alt"${pokemon.name} src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png">
@@ -25,7 +26,7 @@ const fetchPokemon = () =>{
             }, '')//after do the create a li with pokemon data it will be none
 
             const ul = document.querySelector('[data-js="pokedex"]')
-            ul.innerHTML = listPokemon
+            ul.innerHTML = listPokemon// add all content in the ul
         },
 
         )
