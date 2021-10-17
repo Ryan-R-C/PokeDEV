b71a45ea47ef5c1c46fb2a32a58ef57846b18e4f
ryang <ryan.r.c.339ac@gmail.com>
2021-10-17 11:46:42 -0300

    started the app.js getting the pkm url

---
+++ app.js
@@ -0,0 +1,7 @@
+//This function get the pokemon from the PokeApi
+const fetchPokemon = () =>{
+    
+    const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`//GETs API's url
+
+}
+fetchPokemon()
