bed0d9f4a032e218be9664d9535066ab774c1165
ryang <ryan.r.c.339ac@gmail.com>
2021-10-18 04:56:51 -0300

    uncoupled the html generator of the fetchPokemon function

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`//GETs API's url
 //This function get the pokemon from the PokeApi
 
+const generateHTML = pokemons => {
+    return pokemons.reduce((accumulator, pokemon) => {// transforms a array into a string
+        const types = pokemon.types.map(typeinfo => typeinfo.type.name)//gets the type(s) of the pokemon, made it like this to save code
+        //this accumulator will get all the pokemon data and interpolate it in HTML
+        accumulator += `
+            <li class="card ${types[0]}">
+                <img class="card-image" alt"${pokemon.name} src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png">
+                <h2 class="card-title"> ${pokemon.id}.: ${pokemon.name}
+                <p class="card-subtitle">${types.join(" | ")}
+            </li>
+        `
+        return accumulator
+    }, '')
+}
+
 const fetchPokemon = () =>{
     //instead of do a for it could be done using a previous array, and using map to do the hardwork
     generatePokemonPromisses = () => Array(151).fill().map((_, index) => 
@@ -15,26 +30,13 @@ const fetchPokemon = () =>{
         .then(response => response.json())//Change the response from HTTP to JSON. But it so returns a promise
         )//storage the pokemon data in the array*/
         Promise.all(pokemonPromises)//after all pokemon promises be done it will
-        .then(pokemons => {
-            return pokemons.reduce((accumulator, pokemon) => {// transforms a array into a string
-                const types = pokemon.types.map(typeinfo => typeinfo.type.name)//gets the type(s) of the pokemon, made it like this to save code
-                //this accumulator will get all the pokemon data and interpolate it in HTML
-                accumulator += `
-                    <li class="card ${types[0]}">
-                        <img class="card-image" alt"${pokemon.name} src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png">
-                        <h2 class="card-title"> ${pokemon.id}.: ${pokemon.name}
-                        <p class="card-subtitle">${types.join(" | ")}
-                    </li>
-                `
-                return accumulator
-            }, '')//after do the create a li with pokemon data it will be none
-        })
+        .then(generateHTML)//after do the create a li with pokemon data it will be none
         .then(pokemons => {
             const ul = document.querySelector('[data-js="pokedex"]')
             ul.innerHTML = pokemons// add all content in the ul
     },
 
         )
-    }
-
+    
+}
 fetchPokemon()
